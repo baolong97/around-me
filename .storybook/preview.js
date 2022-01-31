6e1e26151bd1602f68cdd941e83a4a8fece6706a
Long Pham <long29031997@gmail.com>
2022-01-31 14:26:58 +0700

    add indicator

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+import { addDecorator } from '@storybook/react';
+import { withThemesProvider } from 'storybook-addon-emotion-theme';
+
+const themes = [{}];
+addDecorator(withThemesProvider(themes));
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  // controls: {
+  //   matchers: {
+  //     color: /(background|color)$/i,
+  //     date: /Date$/,
+  //   },
+  // },
+}
---
+++ src/stories/Indicators.stories.tsx
@@ -0,0 +1,24 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line no-unused-vars
+import { jsx, css } from '@emotion/react'
+import React, { useState } from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Indicators from '../components/Indicators'
+
+export default {
+  title: 'Indicators',
+  component: Indicators
+} as ComponentMeta<typeof Indicators>
+
+const Template: ComponentStory<typeof Indicators> = (args) => {
+  const [currentIdx, setCurrentIdx] = useState(0)
+  return <div css={{ backgroundColor: 'red' }}>
+    <Indicators {...args} currentIdx={currentIdx} onSelect={setCurrentIdx}/>
+  </div>
+}
+
+export const DefaultIndicator = Template.bind({})
+DefaultIndicator.args = {
+  total: 6,
+  currentIdx: 3
+}
